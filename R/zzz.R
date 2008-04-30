0cbd9d4185fdc4647bf4611b71fa7a0c9cb0566f
Roger D. Peng <rpeng@jhsph.edu>
2008-04-30 00:00:00 +0000

    version 0.3-2

--- R/zzz.R
+++ R/zzz.R
@@ -1,35 +1,35 @@
 .onAttach <- function(lib, pkg) {
-    dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-    msg <- gettextf("%s (%s %s)", dcf[, "Title"],
-                    as.character(dcf[, "Version"]), dcf[, "Date"])
-    message(paste(strwrap(msg), collapse = "\n"))    
+        if(!suppressMessages(require(filehash)))
+                stop("'filehash' package required")
+        dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
+        msg <- gettextf("%s (%s %s)", dcf[, "Title"],
+                        as.character(dcf[, "Version"]), dcf[, "Date"])
+        packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
 }
 
 .onLoad <- function(lib, pkg) {
-    pkgList <- c("methods", "filehash")
+        pkgList <- c("methods")
 
-    for(package in pkgList) {
-        if(!require(package, quietly = TRUE, character.only = TRUE))
-            stop(gettextf("'%s' package required", package))
-    }
-    if(!capabilities("http/ftp"))
-        warning("'http/ftp' capabilities not available")
-    stashROption("quietDownload", FALSE)
-    stashROption("offline", FALSE)
-}    
+        for(package in pkgList) {
+                if(!require(package, quietly = TRUE, character.only = TRUE))
+                        stop(gettextf("'%s' package required", package))
+        }
+        if(!capabilities("http/ftp"))
+                warning("'http/ftp' capabilities not available")
+        stashROption("quietDownload", FALSE)
+}
 
 .stashROptions <- new.env()
 
 ## Valid options:
 ##
 ## quietDownload:  Should download progress be shown?
-## offline:  Are we connected to the Internet [not yet implemented]
 
 stashROption <- function(name, value) {
-    if(missing(name))
-        as.list(.stashROptions)
-    else if(missing(value))
-        get(name, .stashROptions, inherits = FALSE)
-    else 
-        assign(name, value, .stashROptions, inherits = FALSE)
+        if(missing(name))
+                as.list(.stashROptions)
+        else if(missing(value))
+                get(name, .stashROptions, inherits = FALSE)
+        else
+                assign(name, value, .stashROptions, inherits = FALSE)
 }
